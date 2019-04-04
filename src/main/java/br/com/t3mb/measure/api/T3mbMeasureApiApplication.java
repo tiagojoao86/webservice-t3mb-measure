313f7f33042979a688e77d09a20231a5d6ecac0b
tiagojoao86 <pereira.tiagojoao@gmail.com>
2019-04-04 11:46:28 -0300

    Initial Version

---
+++ src/main/java/br/com/t3mb/measure/api/T3mbMeasureApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.t3mb.measure.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T3mbMeasureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T3mbMeasureApiApplication.class, args);
+	}
+
+}
